f51eb45c3f5b7f1ff249404286a4aeeff19cd097
refraction-ray <kcanamgal@foxmail.com>
2022-02-21 14:43:59 +0800

    improve template dataset

--- tensorcircuit/templates/dataset.py
+++ tensorcircuit/templates/dataset.py
@@ -2,9 +2,11 @@
 Quantum machine learning related data preprocessing and embedding
 """
 
-from typing import Any, Optional, Sequence
+from typing import Any, Optional, Sequence, Tuple
 
-from ..cons import backend
+import numpy as np
+
+from ..cons import backend, dtypestr
 from ..gates import array_to_tensor
 
 Tensor = Any
@@ -29,7 +31,40 @@ def amplitude_encoding(
     if index is not None:
         index = array_to_tensor(index, dtype="int32")
         fig = backend.gather1d(fig, index)
+    fig = backend.cast(fig, dtypestr)
     return fig
 
 
-batched_amplitude_encoding = backend.vmap(amplitude_encoding, vectorized_argnums=0)
+# batched_amplitude_encoding = backend.vmap(amplitude_encoding, vectorized_argnums=0)
+
+
+def mnist_pair_data(
+    a: int,
+    b: int,
+    binarize: bool = False,
+    threshold: float = 0.4,
+    loader: Any = None,
+) -> Tensor:
+    def filter_pair(x: Tensor, y: Tensor, a: int, b: int) -> Tuple[Tensor, Tensor]:
+        keep = (y == a) | (y == b)
+        x, y = x[keep], y[keep]
+        y = y == a
+        return x, y
+
+    if loader is None:
+        import tensorflow as tf
+
+        loader = tf.keras.datasets.mnist
+
+    (x_train, y_train), (x_test, y_test) = loader.load_data()
+    x_train, x_test = x_train[..., np.newaxis] / 255.0, x_test[..., np.newaxis] / 255.0
+
+    if binarize:
+        x_train[x_train > threshold] = 1.0
+        x_train[x_train <= threshold] = 0.0
+        x_test[x_test > threshold] = 1.0
+        x_test[x_test <= threshold] = 0.0
+
+    x_train, y_train = filter_pair(x_train, y_train, a, b)
+    x_test, y_test = filter_pair(x_test, y_test, a, b)
+    return (x_train, y_train), (x_test, y_test)
