b5e2aaeda6a9052470e1b288a8d24067045f8e81
refraction-ray <kcanamgal@foxmail.com>
2022-06-08 16:12:42 +0800

    version0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## Unreleased
 
+## 0.1.2
+
 ### Added
 
 - Add `rxx`, `ryy` and `rzz` gate
--- tensorcircuit/__init__.py
+++ tensorcircuit/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "TensorCircuit Authors"
 __creator__ = "refraction-ray"
 
