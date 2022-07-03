e5ac962e0c23e04776598d0138f60db93c1cc1c1
refraction-ray <kcanamgal@foxmail.com>
2022-07-03 14:52:12 +0800

    quick fix on develop module leakage

--- tensorcircuit/interfaces/__init__.py
+++ tensorcircuit/interfaces/__init__.py
@@ -6,4 +6,5 @@ from . import tensortrans
 from .tensortrans import numpy_args_to_backend, general_args_to_numpy
 from .scipy import scipy_interface, scipy_optimize_interface
 from .torch import torch_interface, pytorch_interface
-from .tensorflow import tensorflow_interface, tf_interface
+
+# from .tensorflow import tensorflow_interface, tf_interface
