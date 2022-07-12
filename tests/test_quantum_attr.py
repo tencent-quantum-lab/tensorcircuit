a21b0dfe18ab186a1d74134178e0a112202927e0
pramitsingh0 <pramitsingh0@gmail.com>
2022-07-12 17:39:42 +0530

    test keras qaoa quantum_attr

--- tests/test_quantum_attr.py
+++ tests/test_quantum_attr.py
@@ -13,8 +13,8 @@ from tensorcircuit.applications.vags import double_state, reduced_density_matrix
 
 def test_double_state():
     s = double_state(tf.constant([[1.0, 0], [0, -1.0]]), beta=2.0)
-    assert np.allclose(np.linalg.norm(s.numpy()), 1.0)
-    assert np.allclose(
+    np.testing.assert_allclose(np.linalg.norm(s.numpy()), 1.0)
+    np.testing.assert_allclose(
         s.numpy(),
         np.array(
             [
@@ -28,16 +28,16 @@ def test_double_state():
         ),
     )
     s2 = double_state(tf.constant([[0.0, 1.0], [1.0, 0.0]]), beta=1.0)
-    assert np.allclose(np.linalg.norm(s2.numpy()), 1.0)
+    np.testing.assert_allclose(np.linalg.norm(s2.numpy()), 1.0)
     em = np.exp(-0.5)
     ep = np.exp(0.5)
     ans = np.array([em + ep, em - ep, em - ep, em + ep])
     ans /= np.linalg.norm(ans)
-    assert np.allclose(s2.numpy(), ans)
+    np.testing.assert_allclose(s2.numpy(), ans)
 
 
 def test_reduced_dm():
     rho = reduced_density_matrix(
         tf.random.normal(shape=[128]), freedom=7, cut=[1, 3, 5]
     )
-    assert np.allclose(np.trace(rho.numpy()), 1)
+    np.testing.assert_allclose(np.trace(rho.numpy()), 1)
