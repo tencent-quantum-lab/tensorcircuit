41815d12018ce0e93fd2d3fba8aac54b27ef956c
yutuer21 <510465359@qq.com>
2022-12-08 22:50:53 +0800

    revise device readout mitigate2

--- tests/test_results.py
+++ tests/test_results.py
@@ -26,44 +26,43 @@ def test_correlation():
 
 
 def test_readout():
-
     def run(cs, shots):
         nqubit = cs[0]._nqubits
-        gg= []
-        for i in range(2*nqubit):
-            gg.append(np.sin(i)*0.02+0.978)
-        readout_error = np.reshape(gg,(nqubit,2))
+        gg = []
+        for i in range(2 * nqubit):
+            gg.append(np.sin(i) * 0.02 + 0.978)
+        readout_error = np.reshape(gg, (nqubit, 2))
 
         ts = []
         for c in cs:
-            count=c.sample(batch = shots,allow_state=True, readout_error=readout_error,format='count_dict_bin')
+            count = c.sample(
+                batch=shots,
+                allow_state=True,
+                readout_error=readout_error,
+                format="count_dict_bin",
+            )
             ts.append(count)
         return ts
-    
+
     nqubit = 4
-    shots=4096
+    shots = 4096
     c = tc.Circuit(nqubit)
     c.H(0)
     c.cnot(0, 1)
     c.x(3)
-    
-    idea_count = c.sample(batch = shots,allow_state=True, format='count_dict_bin')
-    raw_count = run([c],shots)[0]
+
+    idea_count = c.sample(batch=shots, allow_state=True, format="count_dict_bin")
+    raw_count = run([c], shots)[0]
 
     mit = ReadoutMit(execute=run)
-    mit.cals_from_system([0,1,2,3,6], shots=10000,  method="local")
+    mit.cals_from_system([0, 1, 2, 3, 6], shots=10000, method="local")
 
     # TODO(@yutuer21): add m3 method
-    mit_count1 = mit.apply_correction(raw_count, [1,3,2],method = 'inverse')  #  direct(Max2),iterative(Max3), inverse,square
-    mit_count2 = mit.apply_correction(raw_count, [1,3,2],method = 'square')
-    idea_count2 = counts.marginal_count(idea_count, [1,3,2])
-    
+    mit_count1 = mit.apply_correction(
+        raw_count, [1, 3, 2], method="inverse"
+    )  #  direct(Max2),iterative(Max3), inverse,square
+    mit_count2 = mit.apply_correction(raw_count, [1, 3, 2], method="square")
+    idea_count2 = counts.marginal_count(idea_count, [1, 3, 2])
 
     assert counts.kl_divergence(idea_count2, mit_count1) < 0.05
     assert counts.kl_divergence(idea_count2, mit_count2) < 0.05
-        
-
-
-
-
-
