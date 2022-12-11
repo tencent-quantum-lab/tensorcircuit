df6679561e71cbc3eec1d3f5f2c731b008591a8f
JiaceSun <jsun3@caltech.edu>
2022-12-11 02:43:52 -0800

    update

--- tensorcircuit/circuit.py
+++ tensorcircuit/circuit.py
@@ -412,7 +412,9 @@ class Circuit(BaseCircuit):
                 r = backend.convert_to_tensor(0.0)
             else:
                 r = backend.sum(
-                    backend.stack([backend.sign(x - prob_cumsum[i]) for i in range(l - 1)])
+                    backend.stack(
+                        [backend.sign(x - prob_cumsum[i]) for i in range(l - 1)]
+                    )
                 )
             r = backend.cast(r / 2.0 + (l - 1) / 2.0, dtype="int32")
             # [0: kraus[0], 1: kraus[1]...]
@@ -433,6 +435,7 @@ class Circuit(BaseCircuit):
 
     def _general_kraus_tf(
         self,
+        kraus: Sequence[Gate],
         *index: int,
         status: Optional[float] = None,
     ) -> float:
