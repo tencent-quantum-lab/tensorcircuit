d6a6909879f47698b0988f3544f346f34c012a87
refraction-ray <kcanamgal@foxmail.com>
2023-02-07 14:54:37 +0800

    improve rem example

--- examples/readout_mitigation.py
+++ examples/readout_mitigation.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 import tensorcircuit as tc
-from tensorcircuit.results import counts
 from tensorcircuit.results.readout_mitigation import ReadoutMit
 
 
@@ -22,15 +21,6 @@ def partial_sample(c, batch, readout_error=None):
     return tc.results.counts.marginal_count(ct, measure_index)
 
 
-def run(cs, shots):
-    # customized backend for mitigation test
-    ts = []
-    for c in cs:
-        count = simulator(c, shots)
-        ts.append(count)
-    return ts
-
-
 def simulator(c, shots, logical_physical_mapping=None):
     # with readout_error noise
     nqubit = c._nqubits
@@ -40,12 +30,22 @@ def simulator(c, shots, logical_physical_mapping=None):
     gg = []
     for i in range(200):
         gg.append(np.sin(i) * 0.02 + 0.978)
-        # gg.append(0.98 - i * 0.01)
+        # mocked readout error
     readout_error = np.reshape(gg[0 : nqubit * 2], (nqubit, 2))
     mapped_readout_error = [[1, 1]] * nqubit
     for lq, phyq in logical_physical_mapping.items():
         mapped_readout_error[lq] = readout_error[phyq]
     return partial_sample(c, shots, mapped_readout_error)
+
+
+def run(cs, shots):
+    # customized backend for mitigation test
+    # a simulator with readout error and qubit mapping supporting batch submission
+    ts = []
+    for c in cs:
+        count = simulator(c, shots)
+        ts.append(count)
+    return ts
 
 
 def apply_readout_mitigation():
@@ -57,28 +57,28 @@ def apply_readout_mitigation():
 
     shots = 10000
 
-    idea_count = c.sample(batch=shots, allow_state=True, format="count_dict_bin")
-    raw_count = run([c], 100000)[0]
+    raw_count = run([c], shots)[0]
 
     cal_qubits = [0, 1, 2, 3]
     use_qubits = [0, 1]
 
     # idea_value = c.expectation_ps(z=[0,1])
-    idea_count2 = counts.marginal_count(idea_count, use_qubits)
-    idea_value = counts.expectation(idea_count2, z=[0, 1])
+    idea_count = partial_sample(c, batch=shots)
+    idea_count = tc.results.counts.marginal_count(idea_count, use_qubits)
+    idea_value = tc.results.counts.expectation(idea_count, z=[0, 1])
 
     # use case 1
     mit = ReadoutMit(execute=run)
     mit.cals_from_system(cal_qubits, shots=shots, method="local")
     mit_count = mit.apply_correction(raw_count, use_qubits, method="inverse")
-    mit_value1 = counts.expectation(mit_count, z=[0, 1])
+    mit_value1 = tc.results.counts.expectation(mit_count, z=[0, 1])
 
-    # use case 2
+    # use case 2: directly mitigation on expectation
     mit = ReadoutMit(execute=run)
     mit.cals_from_system(cal_qubits, shots=shots, method="local")
-    mit_value2 = mit.expectation(raw_count, z=[0, 1], method="inverse")
+    mit_value2 = mit.expectation(raw_count, z=[0, 1])
 
-    # use case 3
+    # use case 3: global calibriation
     mit = ReadoutMit(execute=run)
     mit.cals_from_system(cal_qubits, shots=shots, method="global")
     mit_value3 = mit.expectation(raw_count, z=[0, 1], method="square")
