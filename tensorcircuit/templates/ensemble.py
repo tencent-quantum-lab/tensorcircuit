c004c1aba7c980cb6e756f432896ec608b22c547
MarkSong535 <78847784+MarkSong535@users.noreply.github.com>
2023-02-26 17:49:00 +0800

    fix to black

--- tensorcircuit/templates/ensemble.py
+++ tensorcircuit/templates/ensemble.py
@@ -5,27 +5,27 @@ Useful utilities for ensemble
 import tensorflow as tf
 import numpy as np
 
-class bagging: # A.K.A. voting
 
+class bagging:  # A.K.A. voting
     def __init__(self):
         self.models = []
         self.is_trained = []
         self.count = 0
-        self.need_confidence = True # Help in reducing numbers of get_confidence runs
+        self.need_confidence = True  # Help in reducing numbers of get_confidence runs
         self.permit_train = False
 
     def append(self, model, is_trained):
-        '''
+        """
         Add model to the voting method
-        '''
+        """
         self.models.append(model)
         self.is_trained.append(is_trained)
         self.count += 1
 
     def __train_model(self, i, **kwargs):
-        '''
+        """
         Train a model if it isn't trained already
-        '''
+        """
         if not self.is_trained[i]:
             self.need_confidence = True
             self.is_trained[i] = True
@@ -34,86 +34,86 @@ class bagging: # A.K.A. voting
             self.models[i].fit(**kwargs)
 
     def train(self, **kwargs):
-        '''
+        """
         Train all models in the class, **kwargs expect to receive the argus that can be directly sent to tf.fit
-        Expected to be run after finishing compile 
-        '''
+        Expected to be run after finishing compile
+        """
         if not self.permit_train:
             raise Exception("Needed to be compiled before training")
         for i in range(self.count):
             if "verbose" in kwargs:
                 if kwargs["verbose"] == 1:
-                    print("Model ",i+1,"/",self.count," is training...")
+                    print("Model ", i + 1, "/", self.count, " is training...")
             else:
-                print("Model ",i+1,"/",self.count," is training...")
-            self.__train_model(i,**kwargs)
+                print("Model ", i + 1, "/", self.count, " is training...")
+            self.__train_model(i, **kwargs)
 
     def compile(self, **kwargs):
-        '''
+        """
         Compile code
-        '''
+        """
         self.permit_train = True
         for i in range(self.count):
             self.models[i].compile(**kwargs)
 
     def __get_confidence(self, model_index, input):
-        '''
+        """
         Get the confidence value that is needed by voting.
         Number of calling this function is reduced by self.need_confidence
-        '''
+        """
         self.need_confidence = False
-        prediction = self.models[model_index].predict(input,verbose=0)
+        prediction = self.models[model_index].predict(input, verbose=0)
         prediction_returns = np.zeros(len(prediction))
         for i in range(len(prediction)):
             prediction_returns[i] = prediction[i][0]
         return prediction_returns
 
-    '''
+    """
     Voting strategies begin
     More voting strategies can be added beneath, a single function, and a if function in self.predict
-    '''
+    """
 
-    def __voting_weight(self,array):
+    def __voting_weight(self, array):
         result = []
         for i in array:
             result.append(self.__voting_weight_single(i))
         return result
 
-    def __voting_most(self,array):
+    def __voting_most(self, array):
         result = []
         for i in array:
             result.append(self.__voting_most_single(i))
         return result
-    
-    def __voting_average(self,array):
+
+    def __voting_average(self, array):
         result = []
         for i in array:
             result.append(self.__voting_average_single(i))
         return result
 
-    def __voting_weight_single(self,array):
-        opp_array = np.ones(len(array))-array
+    def __voting_weight_single(self, array):
+        opp_array = np.ones(len(array)) - array
         weight = np.absolute(opp_array - array)
         weight_sum = np.sum(weight)
         weight = weight / weight_sum
         result = array * weight
         return np.sum(result)
 
-    def __voting_most_single(self,array):
+    def __voting_most_single(self, array):
         weight = array.size
         result = 0
         for i in array:
-            result += i/weight
+            result += i / weight
         return result
-    
-    def __voting_average_single(self,array):
+
+    def __voting_average_single(self, array):
         result = array / array.size
         return np.sum(result)
 
-    def predict(self, input_data, voting_policy: str="none"):
-        '''
+    def predict(self, input_data, voting_policy: str = "none"):
+        """
         Input data is expected to be a 2D array that the first layer is different input data (into the trained models)
-        '''
+        """
         if self.need_confidence:
             predictions = []
             for i in range(self.count):
@@ -131,15 +131,15 @@ class bagging: # A.K.A. voting
         else:
             raise Exception("voting_policy must be none, weight, most, or average")
 
-    def __acc_binarify(self,array):
-        '''
+    def __acc_binarify(self, array):
+        """
         Needed for ACC test
-        '''
+        """
         result = []
         for i in array:
-            result.append(1 if (i>0.5) else 0)
+            result.append(1 if (i > 0.5) else 0)
         return result
-        
+
     def __eval_accuracy(self, input_data):
         input_data[1] = self.__acc_binarify(input_data[1])
         algo = tf.keras.metrics.Accuracy()
@@ -151,12 +151,12 @@ class bagging: # A.K.A. voting
         algo = tf.keras.metrics.AUC()
         algo.reset_state()
         algo.update_state(input_data[0], input_data[1])
-        return algo.result().numpy() 
-    
-    def eval(self, input_data, evaluation_method: str="acc"):
-        '''
+        return algo.result().numpy()
+
+    def eval(self, input_data, evaluation_method: str = "acc"):
+        """
         Expect input data to be a 2D array, which a 1D array of yTrue followed by a 1D array of yPred is expected to be the components of the 2D array
-        '''
+        """
         if evaluation_method == "acc":
             return self.__eval_accuracy(input_data)
         elif evaluation_method == "auc":
--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -10,52 +10,58 @@ sys.path.insert(0, modulepath)
 
 from tensorcircuit.templates.ensemble import bagging
 
-def test_ensemble_bagging():
 
-    data_amount = 100 # Amount of data to be used
-    linear_demension = 4 # linear demension of the data
+def test_ensemble_bagging():
+    data_amount = 100  # Amount of data to be used
+    linear_demension = 4  # linear demension of the data
     epochs = 10
     batch_size = 32
     lr = 1e-3
 
-    x_train, y_train = (np.ones([data_amount, linear_demension]), np.ones([data_amount, 1]))
+    x_train, y_train = (
+        np.ones([data_amount, linear_demension]),
+        np.ones([data_amount, 1]),
+    )
 
     obj_bagging = bagging()
 
     def model():
-
         DROP = 0.1
 
-        activation = 'selu'
+        activation = "selu"
         inputs = tf.keras.Input(shape=(linear_demension,), name="digits")
-        x0 = tf.keras.layers.Dense(1, 
-                kernel_regularizer = tf.keras.regularizers.l2(9.613e-06),
-                activation = activation,
-                )(inputs)
+        x0 = tf.keras.layers.Dense(
+            1,
+            kernel_regularizer=tf.keras.regularizers.l2(9.613e-06),
+            activation=activation,
+        )(inputs)
         x0 = tf.keras.layers.Dropout(DROP)(x0)
-        
-        x = tf.keras.layers.Dense(1, 
-                    kernel_regularizer = tf.keras.regularizers.l2(1e-07),
-                    activation='sigmoid',
-                    )(x0)
-        
+
+        x = tf.keras.layers.Dense(
+            1,
+            kernel_regularizer=tf.keras.regularizers.l2(1e-07),
+            activation="sigmoid",
+        )(x0)
+
         model = tf.keras.Model(inputs, x)
-    
+
         return model
 
     obj_bagging.append(model(), False)
     obj_bagging.append(model(), False)
     obj_bagging.append(model(), False)
     obj_bagging.compile(
-                    loss=tf.keras.losses.BinaryCrossentropy(),
-                    optimizer=tf.keras.optimizers.Adam(lr),
-                    metrics=[tf.keras.metrics.AUC(),'acc']
-                )
-    obj_bagging.train(x = x_train, y = y_train, epochs = epochs, batch_size = batch_size, verbose = 0)
+        loss=tf.keras.losses.BinaryCrossentropy(),
+        optimizer=tf.keras.optimizers.Adam(lr),
+        metrics=[tf.keras.metrics.AUC(), "acc"],
+    )
+    obj_bagging.train(
+        x=x_train, y=y_train, epochs=epochs, batch_size=batch_size, verbose=0
+    )
 
     v_weight = obj_bagging.predict(x_train, "weight")
     v_most = obj_bagging.predict(x_train, "most")
     v_average = obj_bagging.predict(x_train, "average")
     validation_data = []
-    validation_data.append(obj_bagging.eval([y_train,v_weight],"acc"))
-    validation_data.append(obj_bagging.eval([y_train,v_weight],"auc"))
+    validation_data.append(obj_bagging.eval([y_train, v_weight], "acc"))
+    validation_data.append(obj_bagging.eval([y_train, v_weight], "auc"))
