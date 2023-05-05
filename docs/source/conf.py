d89f30595a30a360afe08b551c067cd9dd1eb07e
refraction-ray <kcanamgal@foxmail.com>
2023-05-05 23:08:30 +0800

    further fix rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,6 +52,8 @@ extensions = [
     "myst_parser",
 ]
 
+nbsphinx_allow_errors = True
+
 autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
