dd8fa81cd8a82ce2c402e957def7d8968f174340
refraction-ray <kcanamgal@foxmail.com>
2023-08-07 21:10:31 +0800

    fix pauli gates

--- tensorcircuit/gates.py
+++ tensorcircuit/gates.py
@@ -13,7 +13,7 @@ import numpy as np
 import tensornetwork as tn
 from scipy.stats import unitary_group
 
-from .cons import backend, dtypestr, npdtype
+from .cons import backend, dtypestr, npdtype, runtime_backend
 from .utils import arg_alias
 
 thismodule = sys.modules[__name__]
@@ -389,7 +389,8 @@ def meta_gate() -> None:
             setattr(thismodule, n + "_gate", temp)
             setattr(thismodule, n, temp)
 
-    setattr(thismodule, "pauli_gates", [i(), x(), y(), z()])  # type: ignore
+    with runtime_backend("numpy"):  # backward compatinility
+        setattr(thismodule, "pauli_gates", [i(), x(), y(), z()])  # type: ignore
 
 
 meta_gate()
