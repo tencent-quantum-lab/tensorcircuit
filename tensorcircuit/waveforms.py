754d5beebb21390073eb2c6405d5d726440bb89b
walterxu <walterxu@tencent.com>
2025-10-20 14:51:10 +0800

    revise waveforms paraemter sequence.

--- tensorcircuit/waveforms.py
+++ tensorcircuit/waveforms.py
@@ -16,82 +16,85 @@ __all__ = [
 
 @dataclass
 class Gaussian:
-    amp: ParamType
     duration: int
+    amp: ParamType
     sigma: ParamType
+    angle: ParamType
     def qasm_name(self) -> str:
         return "gaussian"
     def to_args(self) -> List[ParamType]:
-        return [self.amp, self.duration, self.sigma]
+        return [self.duration, self.amp, self.sigma, self.angle]
 
 @dataclass
 class GaussianSquare:
-    amp: ParamType
     duration: int
+    amp: ParamType
     sigma: ParamType
     width: ParamType
     def qasm_name(self) -> str:
         return "gaussian_square"
     def to_args(self) -> List[ParamType]:
-        return [self.amp, self.duration, self.sigma, self.width]
+        return [self.duration, self.amp, self.sigma, self.width]
 
 @dataclass
 class Drag:
-    amp: ParamType
     duration: int
+    amp: ParamType
     sigma: ParamType
     beta: ParamType
     def qasm_name(self) -> str:
         return "drag"
     def to_args(self) -> List[ParamType]:
-        return [self.amp, self.duration, self.sigma, self.beta]
+        return [self.duration, self.amp, self.sigma, self.beta]
 
 @dataclass
 class Constant:
-    amp: ParamType
     duration: int
+    amp: ParamType
     def qasm_name(self) -> str:
         return "constant"
     def to_args(self) -> List[ParamType]:
-        return [self.amp, self.duration]
+        return [self.duration, self.amp]
 
 @dataclass
 class Sine:
-    amp: ParamType
-    frequency: ParamType
     duration: int
+    amp: ParamType
+    phase: ParamType
+    frequency: ParamType
+    angle: ParamType
     def qasm_name(self) -> str:
-        return "sine"
+        return "sin"
     def to_args(self) -> List[ParamType]:
-        return [self.amp, self.frequency, self.duration]
-
+        return [self.duration, self.amp, self.phase, self.frequency, self.angle]
+    
 @dataclass
 class Cosine:
+    duration: int
     amp: ParamType
     frequency: ParamType
-    duration: int
     def qasm_name(self) -> str:
         return "cosine"
     def to_args(self) -> List[ParamType]:
-        return [self.amp, self.frequency, self.duration]
+        return [self.duration, self.amp, self.frequency]
 
 @dataclass
 class CosineDrag:
-    amp: ParamType
     duration: int
+    amp: ParamType
     phase: ParamType
     alpha: ParamType
     def qasm_name(self) -> str:
         return "cosine_drag"
     def to_args(self) -> List[ParamType]:
-        return [self.amp, self.duration, self.phase, self.alpha]
+        return [self.duration, self.amp, self.phase, self.alpha]
 
 @dataclass
 class Flattop:
+    duration: int
     amp: ParamType
     width: ParamType
-    duration: int
     def qasm_name(self) -> str:
         return "flattop"
     def to_args(self) -> List[ParamType]:
-        return [self.amp, self.width, self.duration]
+        return [self.duration, self.amp, self.width]
